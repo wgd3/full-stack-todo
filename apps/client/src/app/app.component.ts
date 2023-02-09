2a91b0c7f13e3f86844f65cf21fa3df6c9d6a0eb
wgd3 <wallace@thefullstack.engineer>
2023-02-09 13:29:11 -0500

    added client app

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'fse-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
