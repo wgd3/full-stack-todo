7dc8b2c01707f62aa02888f1426c0d6ae504928e
wgd3 <wallace@thefullstack.engineer>
2023-03-07 17:17:28 -0500

    added component outputs

--- libs/client/ui-components/src/lib/to-do/to-do.component.stories.ts
+++ libs/client/ui-components/src/lib/to-do/to-do.component.stories.ts
@@ -7,6 +7,12 @@ import {
 } from '@storybook/angular';
 import { ToDoComponent } from './to-do.component';
 
+/**
+ * Falso's `randTodo` function is perfect, but it wasn't as visually
+ * pleasing to see filler text while designing. Instead, this uses
+ * legitimate English language for the to-do item, and insert a random
+ * `completed` value
+ */
 const randTodo = () => {
   const { id, title, description } = randProduct();
   return {
@@ -30,6 +36,17 @@ export default {
     <div style="width: 500px; height: 280px; position: relative; padding: 2rem">${s}</div>`
     ),
   ],
+  argTypes: {
+    triggerDelete: {
+      action: 'delete',
+    },
+    triggerEdit: {
+      action: 'edit',
+    },
+    triggerToggleComplete: {
+      action: 'toggleComplete',
+    },
+  },
 } as Meta<ToDoComponent>;
 
 export const Primary = {
--- libs/client/ui-components/src/lib/to-do/to-do.component.ts
+++ libs/client/ui-components/src/lib/to-do/to-do.component.ts
@@ -1,16 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
   faCircle as faCircleOutline,
   faTrashCan,
 } from '@fortawesome/free-regular-svg-icons';
-import {
-  faCheck,
-  faCircle,
-  faCircleCheck,
-  faPencil,
-} from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faPencil } from '@fortawesome/free-solid-svg-icons';
 import { ITodo } from '@fst/shared/domain';
 
 @Component({
@@ -24,10 +25,35 @@ import { ITodo } from '@fst/shared/domain';
 export class ToDoComponent {
   @Input() todo: ITodo | undefined;
 
-  completeIcon = faCircleCheck;
-  incompleteIcon = faCircle;
+  @Output() toggleComplete = new EventEmitter<boolean>();
+  @Output() editTodo = new EventEmitter<ITodo>();
+  @Output() deleteTodo = new EventEmitter<ITodo>();
+
   faCheck = faCheck;
   faCircleOutline = faCircleOutline;
   faPencil = faPencil;
   faTrashCan = faTrashCan;
+
+  /**
+   * Simply emit the opposite of the current completed value
+   */
+  triggerToggleComplete() {
+    this.toggleComplete.emit(!this.todo?.completed);
+  }
+
+  /**
+   * Emit the current todo data so that a service or parent component
+   * knows what to work with.
+   */
+  triggerEdit() {
+    this.editTodo.emit(this.todo);
+  }
+
+  /**
+   * Emit the current todo data, although only the ID will most likely
+   * be needed for deletion.
+   */
+  triggerDelete() {
+    this.deleteTodo.emit(this.todo);
+  }
 }
