8d57f66c6e6e1de920c4023868a2a1ecd84d53f9
wgd3 <wallace@thefullstack.engineer>
2023-03-07 16:57:26 -0500

    added buttons to component

--- libs/client/ui-components/src/lib/to-do/to-do.component.ts
+++ libs/client/ui-components/src/lib/to-do/to-do.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCircle as faCircleOutline } from '@fortawesome/free-regular-svg-icons';
+import {
+  faCircle as faCircleOutline,
+  faTrashCan,
+} from '@fortawesome/free-regular-svg-icons';
 import {
   faCheck,
   faCircle,
   faCircleCheck,
+  faPencil,
 } from '@fortawesome/free-solid-svg-icons';
 import { ITodo } from '@fst/shared/domain';
 
@@ -24,4 +28,6 @@ export class ToDoComponent {
   incompleteIcon = faCircle;
   faCheck = faCheck;
   faCircleOutline = faCircleOutline;
+  faPencil = faPencil;
+  faTrashCan = faTrashCan;
 }
