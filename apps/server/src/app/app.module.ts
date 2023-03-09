a67546d91516027947c7e472e0e9f79d8d88cfd4
wgd3 <wallace@thefullstack.engineer>
2023-03-09 12:44:32 -0500

    added docker/compose support to repo

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -17,13 +17,36 @@ import { AppService } from './app.service';
       }),
     }),
     TypeOrmModule.forRootAsync({
-      useFactory: (config: ConfigService) => ({
-        type: 'sqlite',
-        database: config.get('DATABASE_PATH'),
-        synchronize: true,
-        logging: true,
-        autoLoadEntities: true,
-      }),
+      useFactory: (config: ConfigService) => {
+        const env = config.get('ENVIRONMENT') ?? 'development';
+        if (env === 'docker') {
+          console.log(
+            `Detected Docker environment, connecting to docker DB: ${config.get(
+              'DATABASE_HOST'
+            )}`
+          );
+          return {
+            type: config.get('DATABASE_TYPE'),
+            host: config.get('DATABASE_HOST'),
+            username: config.get('DATABASE_USERNAME'),
+            password: config.get('DATABASE_PASSWORD'),
+            port: config.get('DATABASE_PORT'),
+            database: config.get('DATABASE_NAME'),
+            synchronize: true,
+            logging: true,
+            autoLoadEntities: true,
+          } as TypeOrmModuleAsyncOptions; // HERES THE PROBLEM
+        }
+        // default to local devl
+        console.log(`Using SQLite for local dev environment`);
+        return {
+          type: 'sqlite',
+          database: config.get('DATABASE_PATH'),
+          synchronize: true,
+          logging: true,
+          autoLoadEntities: true,
+        };
+      },
       inject: [ConfigService],
     }),
     ServerFeatureTodoModule,
