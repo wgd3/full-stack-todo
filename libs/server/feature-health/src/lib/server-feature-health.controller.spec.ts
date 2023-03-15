fa5603f7933978bcbb829b6e8b382f97649f0bfa
wgd3 <wallace@thefullstack.engineer>
2023-03-15 15:48:01 -0400

    fixed tests

--- libs/server/feature-health/src/lib/server-feature-health.controller.spec.ts
+++ libs/server/feature-health/src/lib/server-feature-health.controller.spec.ts
@@ -1,12 +1,39 @@
+import { LoggerService } from '@nestjs/common';
+import { TerminusModule, TypeOrmHealthIndicator } from '@nestjs/terminus';
+import { HealthCheckExecutor } from '@nestjs/terminus/dist/health-check/health-check-executor.service';
+import { TERMINUS_LOGGER } from '@nestjs/terminus/dist/health-check/logger/logger.provider';
 import { Test } from '@nestjs/testing';
 import { ServerFeatureHealthController } from './server-feature-health.controller';
+
+const loggerMock: Partial<LoggerService> = {
+  log: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  debug: jest.fn(),
+};
+
+const healthCheckExecutorMock: Partial<HealthCheckExecutor> = {
+  execute: jest.fn(),
+};
 
 describe('ServerFeatureHealthController', () => {
   let controller: ServerFeatureHealthController;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [],
+      imports: [TerminusModule],
+      providers: [
+        HealthCheckExecutor,
+        TypeOrmHealthIndicator,
+        {
+          provide: HealthCheckExecutor,
+          useValue: healthCheckExecutorMock,
+        },
+        {
+          provide: TERMINUS_LOGGER,
+          useValue: loggerMock,
+        },
+      ],
       controllers: [ServerFeatureHealthController],
     }).compile();
 
