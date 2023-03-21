77cb3f0864a71159a57331ea27de3c89c397bbab
wgd3 <wallace.daniel3@gmail.com>
2023-03-21 10:48:15 -0400

    removed client e2e tests temporarily

--- apps/client-e2e/src/e2e/app.cy.ts
+++ apps/client-e2e/src/e2e/app.cy.ts
@@ -3,7 +3,7 @@ import { getGreeting } from '../support/app.po';
 describe('client', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  xit('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
