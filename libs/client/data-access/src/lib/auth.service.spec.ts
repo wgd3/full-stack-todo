9bf1d3c79aa799beeba1d11eb89c4edbd9e1931b
wgd3 <wallace.daniel3@gmail.com>
2023-03-27 15:06:47 -0400

    updating tests

--- libs/client/data-access/src/lib/auth.service.spec.ts
+++ libs/client/data-access/src/lib/auth.service.spec.ts
@@ -1,19 +1,66 @@
+import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { TOKEN_STORAGE_KEY } from '@fst/client/util';
 
 import { AuthService } from './auth.service';
 
+const EXPIRED_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndhbGxhY2UyQHRoZWZ1bGxzdGFjay5lbmdpbmVlciIsInN1YiI6ImE5ZjdkOTExLTExNWUtNDRkYy04NjNhLWQyM2MyOGJlMDJkNSIsImlhdCI6MTY3OTkzMzkzNiwiZXhwIjoxNjc5OTM0NTM2fQ.J5NFi_zaSYTYiplDn05OXx0f6gMWWHw7Ki7Hw7kKp3U';
+
 describe('AuthService', () => {
   let service: AuthService;
+  let http: HttpClient;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(AuthService);
+    http = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should update localStorage when setting a token', (done) => {
+    service.setToken('foo');
+    service.accessToken$.subscribe({
+      next: (token) => {
+        expect(token).toEqual('foo');
+        expect(localStorage.getItem(TOKEN_STORAGE_KEY)).toEqual('foo');
+        done();
+      },
+      error: done.fail,
+    });
+  });
+
+  it('should update localStorage when clearing a token', (done) => {
+    service.clearToken();
+    service.accessToken$.subscribe({
+      next: (token) => {
+        expect(token).toBeNull();
+        expect(localStorage.getItem(TOKEN_STORAGE_KEY)).toBeNull();
+        done();
+      },
+      error: done.fail,
+    });
+  });
+
+  it('should load a token from localStorage', (done) => {
+    localStorage.setItem(TOKEN_STORAGE_KEY, EXPIRED_TOKEN);
+    service.loadToken();
+    service.accessToken$.subscribe({
+      next: (val) => {
+        expect(val).toStrictEqual(EXPIRED_TOKEN);
+        done();
+      },
+      error: done.fail,
+    });
+  });
+
+  afterEach(() => {
+    service.clearToken();
   });
 });
--- libs/client/data-access/src/lib/guards/auth.guard.spec.ts
+++ libs/client/data-access/src/lib/guards/auth.guard.spec.ts
@@ -1,17 +1,53 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
-
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../auth.service';
 import { authGuard } from './auth.guard';
 
 describe('authGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
     TestBed.runInInjectionContext(() => authGuard(...guardParameters));
 
+  let router: Router;
+  let auth: AuthService;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AuthService],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+    });
+
+    router = TestBed.inject(Router);
+    auth = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should return true when a token is valid', () => {
+    jest.spyOn(auth, 'isTokenExpired').mockReturnValue(false);
+    expect(
+      executeGuard(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{
+        url: 'testUrl',
+      })
+    ).toBe(true);
+  });
+
+  it('should return false when a token is invalid', () => {
+    jest.spyOn(auth, 'isTokenExpired').mockReturnValue(true);
+    const routerSpy = jest.spyOn(router, 'navigate');
+    expect(
+      executeGuard(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{
+        url: 'testUrl',
+      })
+    ).toEqual(false);
+    expect(routerSpy).toHaveBeenCalled();
   });
 });
