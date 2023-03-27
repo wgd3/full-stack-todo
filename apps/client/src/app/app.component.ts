baac5855eade87037ba7237d38fbb2aea506c715
wgd3 <wallace.daniel3@gmail.com>
2023-03-27 12:35:41 -0400

    added logout functionality

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { RouterModule } from '@angular/router';
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '@fst/client/data-access';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   selector: 'fse-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'client';
+  readonly authService = inject(AuthService);
+  readonly router = inject(Router);
+
+  user$ = this.authService.userData$;
+
+  logout() {
+    this.authService.logoutUser();
+    this.router.navigate([`/login`]);
+  }
 }
