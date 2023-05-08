3bcd3781d419493e5d65f5e0c0bad3aa9a20abe5
wgd3 <wallace@thefullstack.engineer>
2023-05-08 14:57:07 -0400

    added incomplete todo count to header

--- libs/client/ui-components/src/lib/header/header.component.ts
+++ libs/client/ui-components/src/lib/header/header.component.ts
@@ -2,7 +2,8 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import { AuthService } from '@fst/client/data-access';
+import { AuthService, TodoFacade } from '@fst/client/data-access';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'fst-header',
@@ -15,8 +16,13 @@ import { AuthService } from '@fst/client/data-access';
 export class HeaderComponent {
   readonly router = inject(Router);
   readonly authService = inject(AuthService);
+  readonly todoFacade = inject(TodoFacade);
 
   user$ = this.authService.userData$;
+
+  incompleteTodoCount$ = this.todoFacade.todos$.pipe(
+    map((todos) => todos.filter((td) => td.completed === false).length)
+  );
 
   logout() {
     this.authService.logoutUser();
